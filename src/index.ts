953572de766ea2ed2ddd02b6f9eba17092be45a0
AudeizReading <alellouc@student.42nice.fr>
2024-03-07 22:36:10 +0100

    remove babel and decorators configurations
    Wrap the class decorator into à function and it works well

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello World');
+import { logged, loggableClass, loggableMethod } from './decorators';
+
+@loggableClass()
+class Decorated {
+	public constructor() {
+		console.log(`Decorated constructor called`);
+	}
+
+	@logged
+	@loggableMethod('label')
+	public methodDecorated(): void {
+		console.log('Decorated method called');
+	}
+}
+
+const decorated = new Decorated();
+decorated.methodDecorated();
